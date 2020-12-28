4091508668c448c3e1d0077571b9a0077ba3b919
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2020-12-28 16:16:11 +0500

    🎨 Applied Prettifier

--- tests/shorten.test.ts
+++ tests/shorten.test.ts
@@ -1,5 +1,5 @@
-import { shorten } from '../index';
+import { shorten } from '../src/index';
 
-test("Valid URL", () => {
-    expect(shorten(new URL("https://cstayyab.com"))).resolves.toBeInstanceOf(URL);
+test('Valid URL', () => {
+  expect(shorten(new URL('https://cstayyab.com'))).resolves.toBeInstanceOf(URL);
 });
